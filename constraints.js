dfe3e4e36b153658fe08bcfea2e1b6b746bc17e6
Akash1520 <CE2020_107079@Ldceahm.gujgov.edu.in>
2022-03-04 16:07:42 +0530

    constraints in schema

--- constraints.js
+++ constraints.js
@@ -2,7 +2,10 @@ const mongoose = require ('mongoose')
 mongoose.connect("mongodb://localhost:27017/fruitsDB")
 
 const fruitSchema = new mongoose.Schema({
-    name:String,
+    name:{
+        type:String,
+        required:[true,"name me bohot kuchh rakha hain"]
+    },
     rating: {
         type:Number,
         min:1,
@@ -19,7 +22,12 @@ const apple = new Fruit({
     review:"good af"
 })
 
-Fruit.create(apple,function(err,fruit){
+const watermelon = new Fruit({
+    rating:4,
+    review:"good af"
+})
+
+Fruit.create(watermelon,function(err,fruit){
     mongoose.connection.close();
     if(err){
         console.log(err)
